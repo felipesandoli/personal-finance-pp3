3debdadddf9437a960875c1c5ec2d80ceb6921c0
felipesandoli <cfelipesandoli@gmail.com>
2023-01-13 18:19:38 +0000

    Add main function, display menu

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+def main():
+    print("Welcome to your personal finance.\n")
+    print("What would you like to do?\n")
+    print("1. Check balance")
+    print("2. Check expenses summary by category")
+    print("3. Check income summary by category")
+    print("4. Add a new expense")
+    print("5. Add a new income\n")
+    input("Please chosse an option by typing a number from the menu above:")
+
+main()
