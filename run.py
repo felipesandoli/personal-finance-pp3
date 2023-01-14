df83541f11e3e90ab37a8140a1cf62dc656ba0c3
felipesandoli <cfelipesandoli@gmail.com>
2023-01-14 10:06:10 +0000

    Connect to google sheets API following love sandwiches walkthrough project

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+# Connecting to google sheets API following love sandwiches walkthrough project
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT =gspread.authorize(SCOPED_CREDS)
+
+SHEET = GSPREAD_CLIENT.open("personal-finance-pp3")
+
 expenses_categories = {
     "1": "Housing",
     "2": "Transportation",
