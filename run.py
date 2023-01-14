0b0869ffbf392498c23d4cf19ad682c7d27c224e
felipesandoli <cfelipesandoli@gmail.com>
2023-01-14 11:10:29 +0000

    Create a new function for displaying the menu

--- run.py
+++ run.py
@@ -32,6 +32,27 @@ income_categories = {
     "4": "Others"
 }
 
+def display_menu():
+    """
+    Displays the menu of options in the terminal
+    """
+    print("What would you like to do?\n")
+    print("1. Check balance")
+    print("2. Check expenses summary by category")
+    print("3. Check income summary by category")
+    print("4. Add a new expense")
+    print("5. Add a new income")
+    print("6. Exit\n")
+
+    # Validates option, based on love sandwiches walkthrough project
+    while True:
+        choice = input("Please chosse an option by typing a number from the menu above: ")
+
+        if validate_choice(choice):
+            break
+
+    return choice
+
 def calculate_balance():
     """
     Gets data from spreadsheet. Calculates and returns the balance.
@@ -69,41 +90,26 @@ def validate_choice(choice):
     return True
 
 def main():
-    while True:
-        """
-        Displays the menu to the user. After performing an action, the menu is displayed again
-        in a loop until the user decides to exit by choosing the exit option in the menu, where
-        the program will close.
-        """
-        print("Welcome to your personal finance.\n")
-        print("What would you like to do?\n")
-        print("1. Check balance")
-        print("2. Check expenses summary by category")
-        print("3. Check income summary by category")
-        print("4. Add a new expense")
-        print("5. Add a new income")
-        print("6. Exit\n")
+    """
+    Displays the menu to the user. After performing an action, the menu is displayed again
+    in a loop until the user decides to exit by choosing the exit option in the menu, where
+    the program will close.
+    """
+    print("Welcome to your personal finance.\n")
+    choice = display_menu()
 
-        # Validates option, based on love sandwiches walkthrough project
-        while True:
-            choice = input("Please chosse an option by typing a number from the menu above: ")
-
-            if validate_choice(choice):
-                break
-        
-        if int(choice) == 1:
-            balance = calculate_balance()
-            display_balance(balance)
-        elif int(choice) == 2:
-            print("choice: check expenses summary.\n")
-        elif int(choice) == 3:
-            print("choice: check income summary.\n")
-        elif int(choice) == 4:
-            print("choice: add new expense.\n")
-        elif int(choice) == 5:
-            print("choice: add ne income.\n")
-        elif int(choice) == 6:
-            print("Exiting program. Goodbye!")
-            break
+    if int(choice) == 1:
+        balance = calculate_balance()
+        display_balance(balance)
+    elif int(choice) == 2:
+        print("choice: check expenses summary.\n")
+    elif int(choice) == 3:
+        print("choice: check income summary.\n")
+    elif int(choice) == 4:
+        print("choice: add new expense.\n")
+    elif int(choice) == 5:
+        print("choice: add ne income.\n")
+    elif int(choice) == 6:
+        print("Exiting program. Goodbye!")    
 
 main()
