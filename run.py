3940961e5fd4b5735cc84147a9f8b893bd56082e
felipesandoli <cfelipesandoli@gmail.com>
2023-01-17 20:34:11 +0000

    Clear terminal before showing the menu for the second and subsequent times

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 # Connecting to google sheets API following love sandwiches walkthrough project
 import gspread
 from google.oauth2.service_account import Credentials
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT =gspread.authorize(SCOPED_CREDS)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
 SHEET = GSPREAD_CLIENT.open("personal-finance-pp3")
 
@@ -22,15 +23,16 @@ expenses_categories = {
     "5": "Medical & Healthcare",
     "6": "Personal Spending",
     "7": "Leisure",
-    "8": "Others"
+    "8": "Others",
 }
 
 income_categories = {
     "1": "Salary & Wages",
     "2": "Investment returns",
     "3": "Rental Income",
-    "4": "Others"
+    "4": "Others",
 }
+
 
 def display_menu():
     """
@@ -46,12 +48,15 @@ def display_menu():
 
     # Validates option, based on love sandwiches walkthrough project
     while True:
-        choice = input("Please chosse an option by typing a number from the menu above: \n")
+        choice = input(
+            "Please chosse an option by typing a number from the menu above: \n"
+        )
 
         if validate_choice(choice):
             break
 
     return choice
+
 
 def evaluate_choice(choice):
     """
@@ -73,6 +78,7 @@ def evaluate_choice(choice):
     elif int(choice) == 6:
         print("Exiting program. Goodbye!\n")
 
+
 def calculate_balance():
     """
     Gets data from spreadsheet. Calculates and returns the balance.
@@ -87,18 +93,20 @@ def calculate_balance():
         total_income += float(income)
     return total_income - total_expenses
 
+
 def display_balance(balance):
     """
     Displays the current balance to the terminal.
     """
-    print(f"Your current balance is: ${balance:.2f}\n") 
+    print(f"Your current balance is: ${balance:.2f}\n")
+
 
 def calculate_amounts_by_category(type):
     """
     Calculate the total amount spent for each expense category or the total amount of income
     for each income category.
     """
-    amounts = SHEET.worksheet(type).col_values(1) 
+    amounts = SHEET.worksheet(type).col_values(1)
     worksheet_categories = SHEET.worksheet(type).col_values(2)
     if type == "expenses":
         amounts_by_category = {key: 0 for key in expenses_categories.values()}
@@ -112,14 +120,16 @@ def calculate_amounts_by_category(type):
                 amounts_by_category[value] += float(amount)
     return amounts_by_category
 
+
 def display_amounts_by_category(amounts_dict, type):
-    """"
+    """ "
     Displays the total expenses by each category to the terminal
     """
     print(f"The total amount of {type} by each category is:\n")
     for key in amounts_dict:
         print(f"{key}: ${amounts_dict[key]:.2f}")
     print("\n")
+
 
 # Validation method inspired by love sandwiches walkthrough project
 def validate_choice(choice):
@@ -130,12 +140,13 @@ def validate_choice(choice):
     """
     try:
         int(choice)
-        if int(choice) not in range(1,7):
+        if int(choice) not in range(1, 7):
             raise ValueError("Option not found.")
     except ValueError as e:
         print(f"{e}. Try again.")
         return False
     return True
+
 
 def main():
     """
@@ -144,18 +155,20 @@ def main():
     print("Welcome to your personal finance.\n")
     choice = display_menu()
     evaluate_choice(choice)
-    
+
     while True:
         if choice == "6":
             break
         again = input("Would you like to do something else? (Y/N)\n")
         if again.upper() == "Y":
+            os.system("cls")
             choice = display_menu()
             evaluate_choice(choice)
         elif again.upper() == "N":
             print("Exiting program. Goodbye!")
             break
         else:
-            print("Invalid answer, please type Y or N.\n")   
+            print("Invalid answer, please type Y or N.\n")
+
 
 main()
